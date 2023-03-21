ee809e20cf23a98e8cc614146519ff08e928d3b3
pm430 <open.js@gmail.com>
2023-03-21 23:23:26 +0900

    Initial commit

---
+++ src/main/java/org/example/ExcelWriter.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.FileOutputStream;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelWriter {
+    public static void main(String[] args) throws Exception {
+        // 새 엑셀 워크북 생성
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // 첫 번째 시트 생성
+        Sheet sheet = workbook.createSheet("Sheet1");
+
+        // 첫 번째 열 첫 다섯 행에 1 채우기
+        for (int i = 0; i < 5; i++) {
+            Row row = sheet.createRow(i);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(1);
+        }
+
+        // 두 번째 열 첫 행부터 열 줄에 알파벳 채우기
+        for (int i = 0; i < 26; i++) {
+            Row row = sheet.getRow(i % 26);
+            if (row == null) {
+                row = sheet.createRow(i % 26);
+            }
+            Cell cell = row.createCell(1);
+            cell.setCellValue((char) ('a' + i));
+        }
+
+        // 파일에 저장
+        FileOutputStream fileOut = new FileOutputStream("example.xlsx");
+        workbook.write(fileOut);
+        fileOut.close();
+
+        // 메모리에서 제거
+        workbook.close();
+    }
+}
